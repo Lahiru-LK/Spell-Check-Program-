35182b9fa129d0ea61552b573651b57dee2c5d3b
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2024-07-26 23:35:35 +0530

    Update spellcheck.py
    Co-Authored-By: Jack-Been <127935435+jack-been@users.noreply.github.com>

--- spellcheck.py
+++ spellcheck.py
@@ -8,6 +8,7 @@ def read_spellings(filename):
     except FileNotFoundError:
         print(f"Error: '{filename}' not found.")
         exit(1)
+        
     return misspellings
 
 def correct_spelling(input_filename, spell_dict):
